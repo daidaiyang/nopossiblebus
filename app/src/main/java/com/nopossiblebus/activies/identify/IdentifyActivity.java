6eb04e5d5d31020286d96f5b117dea4b98947a6a
daidaiyang—_home <2991233394@qq.com>
2019-02-23 10:35:45 +0800

    初始化界面-首页-提交认证

--- app/src/main/java/com/nopossiblebus/activies/identify/IdentifyActivity.java
+++ app/src/main/java/com/nopossiblebus/activies/identify/IdentifyActivity.java
@@ -1,25 +1,16 @@
 package com.nopossiblebus.activies.identify;
 
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.nopossiblebus.R;
-import com.nopossiblebus.adapter.ConfirOrderItemAdapter;
-import com.nopossiblebus.customview.ShadowDrawable;
 import com.nopossiblebus.mvp.MVPBaseActivity;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -34,6 +25,28 @@ import butterknife.OnClick;
 public class IdentifyActivity extends MVPBaseActivity<IdentifyContract.View, IdentifyPresenter> implements IdentifyContract.View {
 
 
+    @BindView(R.id.title_back)
+    ImageView titleBack;
+    @BindView(R.id.title)
+    TextView title;
+    @BindView(R.id.identify_cancle)
+    TextView cancle;
+    @BindView(R.id.identify_upyingye_rl)
+    RelativeLayout yingyeRl;
+    @BindView(R.id.identify_upyingye_img)
+    ImageView yingyeImg;
+    @BindView(R.id.identify_ypfood_rl)
+    RelativeLayout foodRl;
+    @BindView(R.id.identify_ypfood_img)
+    ImageView foodImg;
+    @BindView(R.id.identify_name)
+    EditText name;
+    @BindView(R.id.identify_tel)
+    EditText tel;
+    @BindView(R.id.identify_merchname)
+    EditText merchname;
+    @BindView(R.id.identify_commit)
+    TextView commit;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,5 +57,25 @@ public class IdentifyActivity extends MVPBaseActivity<IdentifyContract.View, Ide
     }
 
     private void initView() {
+        title.setText("商家认证");
+        titleBack.setVisibility(View.GONE);
+    }
+
+    @OnClick({R.id.identify_cancle, R.id.identify_upyingye_rl, R.id.identify_upyingye_img, R.id.identify_ypfood_rl, R.id.identify_ypfood_img, R.id.identify_commit})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.identify_cancle:
+                break;
+            case R.id.identify_upyingye_rl:
+                break;
+            case R.id.identify_upyingye_img:
+                break;
+            case R.id.identify_ypfood_rl:
+                break;
+            case R.id.identify_ypfood_img:
+                break;
+            case R.id.identify_commit:
+                break;
+        }
     }
 }
